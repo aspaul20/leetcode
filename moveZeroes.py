b05673245203435b9182c7a16cfc2a66dce4a1a9
aspaul20 <87422803+aspaul20@users.noreply.github.com>
2025-02-20 15:58:08 +0500

    Create moveZeroes.py

---
+++ moveZeroes.py
@@ -0,0 +1,18 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        right = 0 
+        while right < len(nums):
+            if nums[right] != 0:
+                nums[right], nums[left] = nums[left], nums[right]
+                left += 1
+            right += 1
+
+        # left = 0 
+        # for right in range(len(nums)):
+        #     if nums[right] != 0:
+        #         nums[left],nums[right] = nums[right],nums[left]
+        #         left += 1
