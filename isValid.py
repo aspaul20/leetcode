b4812bfa5df4e569ca15d6719896a8f10e5ac49d
aspaul20 <87422803+aspaul20@users.noreply.github.com>
2025-02-26 21:56:21 +0500

    Create isValid.py

---
+++ isValid.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mapping = {
+            '}':'{',
+            ']':'[',
+            ')':'('
+        }
+        stack = []
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping.keys():
+                if stack and stack[-1] == mapping[char]:
+                    stack.pop()
+                else:
+                    return False
+        return not stack
